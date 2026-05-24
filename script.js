972047d941b96a6bf55a7ac340a66c14c6013e8c
hasini30 <nadellahasini@gmail.com>
2026-05-24 19:57:07 +0530

    Initial password_generator project setup

---
+++ script.js
@@ -0,0 +1,14 @@
+function generatePassword() {
+    const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+    let password = "";
+
+    for(let i = 0; i < 12; i++) {
+        password += chars.charAt(
+            Math.floor(Math.random() * chars.length)
+        );
+    }
+
+    document.getElementById("password").value = password;
+}
